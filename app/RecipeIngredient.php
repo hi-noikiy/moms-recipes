704f1bfa9267c0cb31319589aa2d66235e00e779
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-05-14 17:55:04 -0400

    make our model extend from pivot class

--- app/RecipeIngredient.php
+++ app/RecipeIngredient.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class RecipeIngredient extends Model
+class RecipeIngredient extends Pivot
 {
-    //
+    protected $table = 'ingredient_recipe';
 }

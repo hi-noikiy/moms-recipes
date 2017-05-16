9a8bd9aaf76f4560327b38967c1d8e9b9e59aab0
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-05-15 22:31:18 -0400

    seed myself into the db

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -7,5 +7,6 @@ class UsersTableSeeder extends Seeder {
     public function run()
     {
         factory('App\User', 10)->create();
+        factory('App\User')->create(['name' => 'Alex Ciarlillo', 'email' => 'alex.ciarlillo@gmail.com', 'password' => bcrypt('password')]);
     }
 }

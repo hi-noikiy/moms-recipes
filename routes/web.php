f21017257171149e823d863497050c033af9b963
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-05-16 20:25:44 -0400

    add routes for index and show recipes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/recipes', 'RecipeController@index');
+Route::get('/recipes/{recipe}', 'RecipeController@show');

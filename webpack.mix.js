10e5f8527314e12d1d9af7e7eeea4266f59486bc
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2018-01-21 17:24:20 -0500

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,54 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+ mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            tailwindcss('./tailwind.js')
+        ]
+    })
+    .sourceMaps()
+    .disableNotifications();
+
+if (mix.inProduction()) {
+    mix.version()
+
+    mix.extract([
+        'vue',
+        'vuex',
+        'axios',
+        'moment',
+        '@fortawesome/fontawesome',
+        '@fortawesome/vue-fontawesome',
+        "@fortawesome/fontawesome-free-brands",
+        "@fortawesome/fontawesome-pro-light",
+        "@fortawesome/fontawesome-pro-regular",
+        "@fortawesome/fontawesome-pro-solid"
+    ]);
+}
+
+mix.webpackConfig({
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+    ],
+    resolve: {
+        alias: {
+            '~': path.join(__dirname, './resources/assets/js')
+        }
+    }
+})
